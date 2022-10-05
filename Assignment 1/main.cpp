a2aa537d44029bcf2a42c01c4d25051208702306
Jerry Shen <terb.avg@gmail.com>
2022-10-05 10:02:43 -0700

    Added AnimalShelter things

---
+++ Assignment 1/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Animals
+{
+private:
+    /* data */
+public:
+    
+};
+
+
+Animals::~Animals()
+{
+}
+
+
+void addAnimal(){
+    string animalName;
+    int catDog;
+    cout << "Please enter the name of your animal:\n";
+    cin >> animalName;
+    cout << "Is it a dog or a cat? (Enter 1 for Dogs, 2 for Cats)\n";
+    cin >> catDog;
+
+    if (catDog == 1)
+    {
+        
+    }
+    
+}
