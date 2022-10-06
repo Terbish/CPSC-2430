bdc115d37a128d24da755aeca468c851559fc1d9
Jerry Shen <terb.avg@gmail.com>
2022-10-06 00:05:13 -0700

    File changes

--- Assignment 1/main.cpp
+++ Assignment 1/main.cpp
@@ -1,33 +1,11 @@
+#include "management.h"
 #include <iostream>
 #include <queue>
+
 using namespace std;
 
-class Animals
-{
-public:
-    queue<string> Dogs;
-    queue<string> Cats;
-};
 
-
-Animals::~Animals()
-{
-}
-
-
-void addAnimal(){
-    string animalName;
-    int catDog;
-    cout << "Please enter the name of your animal:\n";
-    cin >> animalName;
-    cout << "Is it a dog or a cat? (Enter 1 for Dogs, 2 for Cats)\n";
-    cin >> catDog;
-
-    if (catDog == 1)
-    {
-        Dogs.push(animalName);
-    } else if (catDog == 2)
-    {    
-        Cats.push.(animalName);
-    }
+int main() {
+    Init();
+    return 0;
 }
---
+++ Assignment 1/management.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#ifndef _MANAGEMENT_H
+#define _MANAGEMENT_H
+using namespace std;
+
+
+void Init();
+
+#endif
+
